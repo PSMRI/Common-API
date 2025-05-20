936e0bdb1098e398c73dc95d9032b00120084d15
ravishanigarapu <133210792+ravishanigarapu@users.noreply.github.com>
2025-05-20 15:17:22 +0530

    AMM-1456 : JwtToken skiped for mobile app if jwtToken not present (#193)
    * AMM-1378
    
    * AMM-1456
    
    * For addind jwtToken in response

--- src/main/java/com/iemr/common/utils/UserAgentUtil.java
+++ src/main/java/com/iemr/common/utils/UserAgentUtil.java
@@ -4,6 +4,6 @@ public class UserAgentUtil {
     public static boolean isMobileDevice(String userAgent) {
         if (userAgent == null) return false;
         String lowerUA = userAgent.toLowerCase();
-        return lowerUA.contains("mobile") || lowerUA.contains("android") || lowerUA.contains("iphone");
+        return lowerUA.contains("okhttp");
     }
 }
