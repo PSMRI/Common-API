6fc56fae15fa7fb1258862ab3f97b6e6a863258e
Ravi Shanigarapu <ravi.shanigarapu@wipro.com>
2025-05-23 18:08:28 +0530

    Equalignorecase added

--- src/main/java/com/iemr/common/controller/users/IEMRAdminController.java
+++ src/main/java/com/iemr/common/controller/users/IEMRAdminController.java
@@ -1093,7 +1093,7 @@ public class IEMRAdminController {
 
 		if (cookies != null) {
 			for (Cookie cookie : cookies) {
-				if ("jwtToken".equals(cookie.getName())) {
+				if ("Jwttoken".equalsIgnoreCase(cookie.getName())) {
 					String jwtToken = cookie.getValue();
 					// Return the JWT token in the response
 					return ResponseEntity.ok(jwtToken);
