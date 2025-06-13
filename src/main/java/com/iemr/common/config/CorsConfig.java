89c5602b8f9a19b4171558d7d09c3a906b67c58c
vishwab1 <vishwanath@navadhiti.com>
2025-06-13 14:21:38 +0530

    fix: cors config change

--- src/main/java/com/iemr/common/config/CorsConfig.java
+++ src/main/java/com/iemr/common/config/CorsConfig.java
@@ -3,6 +3,7 @@ package com.iemr.common.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
@@ -14,7 +15,10 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns(allowedOrigins.split(","))
+                .allowedOriginPatterns(
+                        Arrays.stream(allowedOrigins.split(","))
+                                .map(String::trim)
+                                .toArray(String[]::new))
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization", "Jwttoken")
