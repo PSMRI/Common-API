bddf0dc16c41a01800462661f64d086437b46c2b
Saurav Mishra <saurav.mishra@bizbrolly.com>
2025-06-19 13:04:41 +0530

    Firebase update userToken

--- src/main/java/com/iemr/common/repo/userToken/UserTokenRepo.java
+++ src/main/java/com/iemr/common/repo/userToken/UserTokenRepo.java
@@ -22,25 +22,10 @@
 /*
 * AMRIT – Accessible Medical Records via Integrated Technology
 */
-package com.iemr.common.data.userToken;
+package com.iemr.common.repo.userToken;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.Data;
+import com.iemr.common.data.userToken.UserTokenData;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.sql.Timestamp;
-
-@Entity
-@Table(name = "user_tokens", schema = "db_iemr")
-@Data
-public class UserTokenData {
-    @Id
-    @Column(name = "user_id")
-    Integer userId;
-    @Column(name = "token")
-    String token;
-    @Column(name = "updated_at")
-    Timestamp updatedAt;
+public interface UserTokenRepo extends JpaRepository<UserTokenData,Integer> {
 }
