dff88e6842ba0f06c5a654d1f64ca70d3ffd473c
Saurav Mishra <saurav.mishra@bizbrolly.com>
2025-07-16 10:06:29 +0530

    dynamic from feature

--- src/main/java/com/iemr/common/repository/dynamic_form/FormTypeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.iemr.common.repository.dynamic_form;
-
-import com.iemr.common.data.dynamic_from.FormEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FormTypeRepository extends CrudRepository<FormEntity, Long> {}
