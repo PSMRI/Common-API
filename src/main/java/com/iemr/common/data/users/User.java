6bd8475530ebd4b9ca28d0ba63e811b75264e460
vishwab1 <vishwanath@navadhiti.com>
2025-08-01 13:49:10 +0530

    fix: remove user mapping

--- src/main/java/com/iemr/common/data/users/User.java
+++ src/main/java/com/iemr/common/data/users/User.java
@@ -76,7 +76,7 @@ public class User implements Serializable  {
 
 	@Expose
 	// @Transient
-	@OneToMany(fetch = FetchType.EAGER)
+	@OneToMany(/* mappedBy = "m_user", fetch = FetchType.EAGER */)
 	@JoinColumn(updatable = false, insertable = false, name = "userID", referencedColumnName = "userID")
 	private Set<UserLangMapping> m_UserLangMappings;
 
