07cccb091a0246a376758862f9117258670b7dd0
Vanitha <vanitha@navadhiti.com>
2025-08-20 13:19:21 +0530

    fix: add logger

--- src/main/java/com/iemr/common/utils/JwtUserIdValidationFilter.java
+++ src/main/java/com/iemr/common/utils/JwtUserIdValidationFilter.java
@@ -150,6 +150,7 @@ public class JwtUserIdValidationFilter implements Filter {
 		if (userAgent == null)
 			return false;
 		userAgent = userAgent.toLowerCase();
+		logger.info("User agent="+userAgent);
 		// return userAgent.contains("okhttp"); // iOS (custom clients)
 		return userAgent.contains("okhttp") || userAgent.contains("java/"); // iOS (custom clients)
 	}
