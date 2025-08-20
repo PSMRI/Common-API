b9549b5cf6d4c51d2ba7c061c2b56dd796879018
Vanitha <vanitha@navadhiti.com>
2025-08-20 12:57:55 +0530

    fix: add useragent as java to skip the token

--- src/main/java/com/iemr/common/utils/JwtUserIdValidationFilter.java
+++ src/main/java/com/iemr/common/utils/JwtUserIdValidationFilter.java
@@ -150,7 +150,8 @@ public class JwtUserIdValidationFilter implements Filter {
 		if (userAgent == null)
 			return false;
 		userAgent = userAgent.toLowerCase();
-		return userAgent.contains("okhttp"); // iOS (custom clients)
+		// return userAgent.contains("okhttp"); // iOS (custom clients)
+		return userAgent.contains("okhttp") || userAgent.contains("java/"); // iOS (custom clients)
 	}
 
 	private boolean shouldSkipAuthentication(String path, String contextPath) {
