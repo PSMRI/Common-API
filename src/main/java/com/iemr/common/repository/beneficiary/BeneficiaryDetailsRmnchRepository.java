0dfad088859771bd00dece45c6d7c376e44593a3
Saurav Mishra <saurav.mishra@bizbrolly.com>
2025-09-17 18:12:58 +0530

    fix strict-origin-when-cross-origin

--- src/main/java/com/iemr/common/repository/beneficiary/BeneficiaryDetailsRmnchRepository.java
+++ src/main/java/com/iemr/common/repository/beneficiary/BeneficiaryDetailsRmnchRepository.java
@@ -2,7 +2,9 @@ package com.iemr.common.repository.beneficiary;
 
 import com.iemr.common.model.beneficiary.RMNCHBeneficiaryDetailsRmnch;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BeneficiaryDetailsRmnchRepository extends JpaRepository<RMNCHBeneficiaryDetailsRmnch,Long> {
 
 }
