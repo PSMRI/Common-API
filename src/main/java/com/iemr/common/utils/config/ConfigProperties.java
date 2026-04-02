4f52a9fab9e995c7a969f9eb108677c1c08701d5
vishwab1 <vishwanath@navadhiti.com>
2026-04-02 16:29:39 +0530

    fix:view file correction

--- src/main/java/com/iemr/common/utils/config/ConfigProperties.java
+++ src/main/java/com/iemr/common/utils/config/ConfigProperties.java
@@ -144,14 +144,27 @@ public class ConfigProperties
 		String result = null;
 		try
 		{
-			if (properties == null)
+			if (environment != null)
 			{
-				initalizeProperties();
+				result = environment.getProperty(propertyName);
 			}
-			result = properties.getProperty(propertyName).trim();
+			if (result == null)
+			{
+				if (properties == null)
+				{
+					initalizeProperties();
+				}
+				result = properties.getProperty(propertyName).trim();
+			} else
+			{
+				result = result.trim();
+			}
 		} catch (Exception e)
 		{
 			logger.error(propertyName + " retrival failed.", e);
+		}
+		if (propertyName.startsWith("km-")) {
+			logger.info("getPropertyByName - " + propertyName + " = " + (result != null ? result : "null") + " | env=" + (environment != null));
 		}
 		return result;
 	}
